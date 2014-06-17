8775e881cb3c81f0c5a103336ecd1aae4f6e738a
Aislan Maia <aislan.sousamaia@gmail.com>
2014-06-17 05:23:52 -0300

    Adicionado template para cabeçalho do conteudo e breadcrumb. Adicionado templates para criar perguntas e para a página da pergunta.

--- model/questions.js
+++ model/questions.js
@@ -1,1 +1,13 @@
 Questions = new Meteor.Collection('questions');
+
+Meteor.methods({
+  question: function (attributes) {
+    var question = _.extend(attributes, 'title', 'text', {
+      submitted: new Date().getTime()
+    });
+
+    questionId = Questions.insert(question);
+
+    return questionId;
+  }
+});
