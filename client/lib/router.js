b2cd0b7e5f2b55c650fad1ef401de7536ffa82fe
Aislan Maia <aislan.sousamaia@gmail.com>
2014-06-20 15:40:43 -0300

    Pequena refatoracao em questions_list, router.js. Acrescentado arquivo misc.less para estilizacao personalidade de coisas comuns ao projeto.

--- client/lib/router.js
+++ client/lib/router.js
@@ -9,32 +9,47 @@ Router.map(function () {
     layoutTemplate: 'layout'
   });
 
-  this.route('question_page', {
-    path: '/questions/:_id',
-    layoutTemplate: 'layout',
-    waitOn: function () {
-      return [
-        Meteor.subscribe('question', this.params._id)
-      ];
-    },
-    data: function () {
-      return Questions.findOne(this.params._id);
-    }
-  });
-
   this.route('questions_list', {
     path: '/questions/',
     layoutTemplate: 'layout',
     waitOn: function () {
       return [
         Meteor.subscribe('user_questions', Meteor.userId()),
+      ];
+    },
+    data: function () {
+      return {
+        questions: Questions.find({author_id: Meteor.userId()})
+      };
+    }
+  });
+
+  this.route('questions_list_followed', {
+    path: '/questions/followed/',
+    layoutTemplate: 'layout',
+    template: 'questions_list',
+    waitOn: function () {
+      return [
         Meteor.subscribe('questions_followed', Meteor.userId())
       ];
     },
     data: function () {
       return {
-        questions: Questions.find({author_id: Meteor.userId()}).fetch()
+        questions: QuestionsFollowed.find({user_id: Meteor.userId()})
       };
     }
   });
-});
+
+  this.route('question_page', {
+      path: '/questions/:_id',
+      layoutTemplate: 'layout',
+      waitOn: function () {
+        return [
+          Meteor.subscribe('question', this.params._id)
+        ];
+      },
+      data: function () {
+        return Questions.findOne(this.params._id);
+      }
+    });
+  });
